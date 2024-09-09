610b63b1ee8af9d4a82f081592ef15725ebe9563
AASTHA RAWAT <aastharawat@microsoft.com>
2024-09-09 16:55:43 +0530

    Debug

--- lisa/sut_orchestrator/libvirt/platform.py
+++ lisa/sut_orchestrator/libvirt/platform.py
@@ -555,26 +555,15 @@ class BaseLibvirtPlatform(Platform, IBaseLibvirtPlatform):
         for node in environment.nodes.list():
             self._log.debug(f"==>node: {node.name}")
             if node.capability.features:
-                # self._log.debug(f"==>node.capability.features: {node.capability.features}")
-                # for setting in node.capability.features.items:
-                #     if setting.type not in features_settings:
-                #         # reload to type specified settings
-                #         try:
-                #             settings_type = feature.get_feature_settings_type_by_name(
-                #                 setting.type, BaseLibvirtPlatform.supported_features()
-                #             )
-                #         except NotMeetRequirementException as identifier:
-                #             raise LisaException(
-                #                 f"platform doesn't support all features. {identifier}"
-                #             )
-                #         new_setting = schema.load_by_type(settings_type, setting)
-                #         features_settings[setting.type] = new_setting
-                new_settings = search_space.SetSpace[schema.FeatureSettings](is_allow_set=True)
+                new_settings = search_space.SetSpace[schema.FeatureSettings](
+                    is_allow_set=True
+                )
                 for current_settings in node.capability.features.items:
                     # reload to type specified settings
                     try:
                         settings_type = feature.get_feature_settings_type_by_name(
-                            current_settings.type, BaseLibvirtPlatform.supported_features()
+                            current_settings.type, 
+                            BaseLibvirtPlatform.supported_features()
                         )
                     except NotMeetRequirementException as identifier:
                         raise LisaException(
@@ -587,10 +576,9 @@ class BaseLibvirtPlatform(Platform, IBaseLibvirtPlatform):
                     if existing_setting:
                         new_settings.remove(existing_setting)
                         new_setting = existing_setting.intersect(new_setting)
-                    new_settings.add(new_setting)  
+                    new_settings.add(new_setting)
                 node.capability.features = new_settings
 
-        # for f in features_settings.values():
         for f in node.capability.features:
             feature_type = next(
                 x for x in self.supported_features() if x.name() == f.type
