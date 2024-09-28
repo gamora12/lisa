2a483a0f02e17c095fd909087104688c98a981fd
AASTHA RAWAT <aastharawat@microsoft.com>
2024-09-28 18:48:04 +0530

    Change guest_vm_type to type enum

--- lisa/sut_orchestrator/libvirt/ch_platform.py
+++ lisa/sut_orchestrator/libvirt/ch_platform.py
@@ -13,8 +13,7 @@ from lisa.environment import Environment
 from lisa.feature import Feature
 from lisa.node import Node
 from lisa.sut_orchestrator.libvirt.context import (
-    GuestVmType, 
-    NodeContext, 
+    NodeContext,
     get_node_context,
 )
 from lisa.sut_orchestrator.libvirt.platform import BaseLibvirtPlatform
