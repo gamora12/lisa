f7f29a61ea54eaf0725e0c2e9cfa692fe6f71067
Aastha Rawat <aastharawat@microsoft.com>
2025-12-16 19:09:17 +0530

    disable building docs

--- lisa/sut_orchestrator/libvirt/transformers.py
+++ lisa/sut_orchestrator/libvirt/transformers.py
@@ -326,7 +326,8 @@ class LibvirtSourceInstaller(LibvirtInstaller):
 
     def _build_and_install(self, code_path: PurePath) -> None:
         self._node.execute(
-            "meson build -D driver_ch=enabled -D driver_qemu=disabled \\"
+            "meson setup build -Ddocs=disabled -Dman=disabled "
+            "-D driver_ch=enabled -D driver_qemu=disabled \\"
             "-D driver_openvz=disabled -D driver_esx=disabled \\"
             "-D driver_vmware=disabled  -D driver_lxc=disabled \\"
             "-D driver_libxl=disabled -D driver_vbox=disabled \\"
